5dd1f03a7154956d3d3814689eb23c7598631249
domotord90 <jazzmaster@freemail.com>
2019-11-16 18:29:48 +0100

    Updates

---
+++ precache-manifest.d09393270cec3514d52d667797a1866a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7d4c2719c16b568f15ec240bfa4e533",
+    "url": "/react-quote-machine/index.html"
+  },
+  {
+    "revision": "2aa424a25ce2e9c20f9d",
+    "url": "/react-quote-machine/static/css/main.fe88ebfd.chunk.css"
+  },
+  {
+    "revision": "7c354cd59cdaeaab20ea",
+    "url": "/react-quote-machine/static/js/2.b5e6158c.chunk.js"
+  },
+  {
+    "revision": "2aa424a25ce2e9c20f9d",
+    "url": "/react-quote-machine/static/js/main.61f5c595.chunk.js"
+  },
+  {
+    "revision": "d577ffe1b9beecf172de",
+    "url": "/react-quote-machine/static/js/runtime~main.fc156fd4.js"
+  }
+]);
